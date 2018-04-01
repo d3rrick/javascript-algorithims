bf93bff12adc52f1731efd3f86be040c99c1601f
muriithiderro <muriithiderrick56@gmail.com>
2018-04-01 12:32:43 +0300

    added conditionals

--- javascript_factorial_algorithim.js
+++ javascript_factorial_algorithim.js
@@ -1,25 +1,21 @@
 // factorial algorithim without iteration//
 
 var limit =1;
+
 function factorial(x){
-    
     // this creates a positive integer
 	var y = Math.abs(x);
 	if(y==0||y==1){return 1}
     
 	var result = y
 	while(y!=limit){
-		y--
-		result = y*result
+		y--;
+		result = y*result;
 	}
-	// this checks if the original number had a positive or negative
-	if (Math.sign(x) == 1){
-		return result
-	}else{
-		return -result
-	}
+
+	return Math.sign(x) == 1?result: -result;
 	
 }
  // test out with both positive and negative numbers.
-console.log(factorial(4));   ans = 24
-console.log(factorial(-4));   ans = -24
+console.log(factorial(-4));   ans = 24
+console.log(factorial(4));   ans = -24
